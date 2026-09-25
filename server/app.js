c652137a2f76dcec3cfdebdf6a7e2df3c48268e4
AbdulArfaz <abdularfaz00@gmail.com>
2026-09-26 01:11:45 +0530

    added origin in cors

--- server/app.js
+++ server/app.js
@@ -10,22 +10,26 @@ import { initScheduler } from "./services/scheduler.services.js";
 
 const app = express();
 
-app.use(cors({
-    origin: 'https://social-media-automation-using-ai.onrender.com',
-    credentials: true
-})
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://social-media-automation-using-ai-bice.vercel.app",
+    ],
+    credentials: true,
+  })
 );
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/users", userRouter);
-app.use("/api/oauth", socialAuthRouter)
-app.use("/api/accounts", accountRouter)
-app.use("/api/posts", postRouter)
-app.use('/api/activity', activityRouter)
+app.use("/api/oauth", socialAuthRouter);
+app.use("/api/accounts", accountRouter);
+app.use("/api/posts", postRouter);
+app.use("/api/activity", activityRouter);
 
-initScheduler()
+initScheduler();
 
 app.get("/", (req, res) => {
   res.send("Server is working");
@@ -36,7 +40,7 @@ app.use((err, req, res, next) => {
   const message = err.message || "Internal Server Error";
 
   if (statusCode !== 401) {
-    console.log(err)
+    console.log(err);
   }
 
   return res.status(statusCode).json({
@@ -46,6 +50,5 @@ app.use((err, req, res, next) => {
     errors: err.errors || [],
   });
 });
-
 
 export { app };
